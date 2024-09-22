0276fd6ea59d22897b63616ba0b21cad54a72818
Gjum <code.gjum@gmail.com>
2024-09-22 06:36:01 +0200

    1.21 update

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
 # MC-Icons
 
 ```css
-/* blocks: */
 background-image: url("https://mc-icons.netlify.app/icons/${id}.png");
 background-size: contain;
-
-/* items: */
-background-image: url("https://mc-icons.netlify.app/items_atlas.png");
-background-position: ${offset};
 image-rendering: pixelated;
 ```
 
@@ -15,10 +10,7 @@ Full example: [tech-tree-explorer](https://github.com/CivPlatform/tech-tree-expl
 
 ---
 
-https://mc-icons.netlify.app/item_infos.json maps block/item `id` (in lower_snake_case) to their display `name`; items also map to an `offset`.
+https://mc-icons.netlify.app/item_infos.json maps block/item `id` (in lower_snake_case) to its display `name`.
 
-All item icons are in one big atlas image; the `offset` is used to select one icon.
-This means the element must be sized 16x16 px, or adjacent icons will be visible.
-
-Block icons are from https://minecraft.fandom.com/wiki/Block  
-Item icons are from https://minecraft.fandom.com/wiki/Item
+Block icons are from https://minecraft.wiki/w/Block  
+Item icons are from https://minecraft.wiki/w/Item
--- main.js
+++ main.js
@@ -1,59 +1,66 @@
-/*
-Items: from https://minecraft.fandom.com/wiki/Item
-
-```css
-background-image: url(https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/f5/ItemCSS.png/revision/latest?cb=20220125120756&amp;version=1643112327234&amp;format=original);
-background-position: -32px -240px;
-```
-
-Blocks: from https://minecraft.fandom.com/wiki/Block
-
-```
-https://static.wikia.nocookie.net/minecraft_gamepedia/images/${url_part}.png/revision/latest/scale-to-height-down/64
-```
-*/
-
 const fs = require("fs");
 const https = require("https");
 
 const capitalize = (s) => s[0].toUpperCase() + s.substring(1);
 
 const pm_items = JSON.parse(fs.readFileSync("data/items.json"));
-/** @type {{[name:string]:string}} */
-const names_by_id = {};
+/** @type {Map<string,string>} */
+const names_by_id = new Map();
 for (let { name: id, displayName: name } of pm_items) {
-	if (names_by_id[id])
-		throw new Error(`Duplicate id '${id}' '${names_by_id[id]}' '${name}'`);
-	// fix outdated names
-	if (name === "Banner Pattern") name = id.split("_").map(capitalize).join(" ");
-	names_by_id[id] = name;
+	if (names_by_id.has(id))
+		throw new Error(`Duplicate id '${id}' '${names_by_id.get(id)}' '${name}'`);
+	// fix reused names
+	if (name === "Music Disc") name = id.split("_").map(capitalize).join(" ");
+	if (name === "Smithing Template")
+		name = id.split("_").map(capitalize).join(" ");
+	names_by_id.set(id, name);
 }
 
-// fix outdated names
-names_by_id["light"] = "Light Block";
-names_by_id["music_disc_otherside"] = "Otherside Disc";
-names_by_id["music_disc_pigstep"] = "Pigstep Disc";
-
-/** @type {{[name:string]:string}} */
-const item_offsets_by_name = JSON.parse(
-	fs.readFileSync("data/item_offsets.json")
+/*
+https://minecraft.fandom.com/wiki/Block
+```js
+imgs = document.querySelectorAll("ul>li img.hoverZoomLink.mw-file-element");
+block_imgs_by_name = {};
+for (let i = 0; i < imgs.length; ++i) {
+	e = imgs[i].parentElement.parentElement.parentElement;
+	name = e.querySelector("a[title]").innerText.replace(' (block)', '');
+	if (name === 'Light Block') name = 'Light';
+	if (block_imgs_by_name[name]) continue; // skip legacy
+	block_imgs_by_name[name] = e.src.replace("/thumb", "").replace(/\.png.+/, ".png");
+}
+console.log(JSON.stringify(block_imgs_by_name).replaceAll('","', '",\n"'));
+```
+*/
+const block_imgs_by_name = JSON.parse(
+	fs.readFileSync("data/block_imgs_by_name.json")
 );
 
-/** @type {{[name:string]:string}} */
-const block_imgs_by_name = JSON.parse(
-	fs.readFileSync("data/block_imgs.json")
-).by_name;
-
 const item_infos = {};
-const imgs_to_download = {};
-for (const [id, name] of Object.entries(names_by_id)) {
-	if (item_offsets_by_name[name]) {
-		item_infos[id] = { name, offset: item_offsets_by_name[name] };
-	} else if (block_imgs_by_name[name]) {
-		imgs_to_download[id] = block_imgs_by_name[name];
-		item_infos[id] = { name };
-	} else
-		console.error(`No img/offset(wiki) for name(pm): ${name} id(pm): ${id}`);
+const imgs_to_download = new Map();
+for (let [id, name] of names_by_id.entries()) {
+	item_infos[id] = { name };
+
+	if (block_imgs_by_name[name]) {
+		imgs_to_download.set(id, block_imgs_by_name[name]);
+	} else {
+		// representatives
+		if (name.includes("Potion")) name = name.replace("Potion", "Water Bottle");
+		if (id === "tipped_arrow") name = "Arrow of Splashing";
+		if (id === "debug_stick") name = "Stick";
+		// misnamed files
+		if (id === "golden_horse_armor") name = name.replace("Golden", "Gold");
+		if (id === "honey_bottle") name = "Honey";
+		if (id.endsWith("_smithing_template"))
+			name = name.replace(" Smithing Template", "");
+		if (id === "shaper_armor_trim_smithing_template") name = "Armor Trim";
+
+		const dashed = name
+			.toLowerCase()
+			.replaceAll(" ", "-")
+			.replaceAll("'", "%27");
+		let url = `https://minecraft.wiki/images/ItemSprite_${dashed}.png`;
+		imgs_to_download.set(id, url);
+	}
 }
 
 fs.mkdirSync("public/", { recursive: true });
@@ -63,27 +70,25 @@ fs.writeFileSync(
 	JSON.stringify(item_infos).replaceAll("},", "},\n")
 );
 
-const atlasUrl =
-	"https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/f5/ItemCSS.png/revision/latest?cb=20220125120756&amp;version=1643112327234&amp;format=original";
-https.get(atlasUrl, (res) => {
-	const path = `public/items_atlas.png`;
-	const ws = fs.createWriteStream(path);
-	res.pipe(ws);
-	ws.on("finish", () => {
-		ws.close();
-	});
-});
-
 fs.mkdirSync("public/icons/", { recursive: true });
 
-for (const [id, img] of Object.entries(imgs_to_download)) {
-	const url = `https://static.wikia.nocookie.net/minecraft_gamepedia/images/${img}.png/revision/latest/scale-to-height-down/64`;
-	https.get(url, (res) => {
-		const path = `public/icons/${id}.png`;
-		const ws = fs.createWriteStream(path);
-		res.pipe(ws);
-		ws.on("finish", () => {
-			ws.close();
-		});
-	});
-}
+(async () => {
+	const jobs = [...imgs_to_download.entries()].sort();
+	const batchSize = 30;
+	for (let i = 0; i < jobs.length; i += batchSize) {
+		const promises = [];
+		for (let j = 0; j < batchSize; j++) {
+			if (i + j >= jobs.length) break;
+			const [id, url] = jobs[i + j];
+			const path = `public/icons/${id}.png`;
+			if (fs.existsSync(path)) continue;
+			// console.log(path, url);
+			promises.push(
+				fetch(url)
+					.then((res) => res.arrayBuffer())
+					.then((data) => fs.writeFileSync(path, Buffer.from(data)))
+			);
+		}
+		await Promise.all(promises);
+	}
+})();
